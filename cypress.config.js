ac8730674dc29fe9b2da667799490ab0a7e6ac23
bakelly <bakelly@ncsu.edu>
2023-12-28 15:15:57 -0500

    Added wait to serching test

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,16 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    baseUrl: 'https://magento.softwaretestingboard.com',
+  },
+
+  reporter: 'mochawesome',
+
+  reporterOptions: {
+    'reportDir': 'cypress/results',
+    'overwrite': false,
+    'html': true,
+    'json': false
   },
 });
