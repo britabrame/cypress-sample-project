7ce6a6809a10d226ca6e43feb1c927fb7e066633
bakelly <bakelly@ncsu.edu>
2024-02-02 12:32:45 -0500

    implemented POM design

--- cypress.config.js
+++ cypress.config.js
@@ -14,5 +14,14 @@ module.exports = defineConfig({
     'json': true
   },
 
-  video: true
+  video: true,
+
+  retries: {
+    // Configure retry attempts for `cypress run`
+    // Default is 0
+    runMode: 2,
+    // Configure retry attempts for `cypress open`
+    // Default is 0
+    openMode: 2
+  }
 });
